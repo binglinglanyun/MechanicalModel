934ad6f6c7776c2a599f19542a010650400a9691
Xiangnan Li <xnl@microsoft.com>
2018-09-25 23:44:36 +0800

    1. Split KongSun and HengNiuJu 2. Add surface

---
+++ MechanicalModel/ViewModels/JiSuanType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MechanicalModel.ViewModels
+{
+    public enum JiSuanType
+    {
+        Others,
+        KongSuanJiSuan,             // 空损计算
+        HengNiuJuJiSuan,            // 制动特性计算
+    }
+}
--- MechanicalModel/ViewModels/ViewType.cs
+++ MechanicalModel/ViewModels/ViewType.cs
@@ -14,11 +14,11 @@ namespace MechanicalModel.ViewModels
         ShuXueKongSunJiSuanCanShuShuRu,       // 数学模型 --> 空损计算 --> 计算参数输入
         ShuXueKongSunJiSuan,                  // 数学模型 --> 空损计算 --> 计算
         ShuXueKongSunJiSuanJieGuoShuChu,      // 数学模型 --> 空损计算 --> 计算参数输出
-        ShuXueZhiDongNiuJuPiPeiTeXingGuanXi,  // 数学模型 --> 恒扭矩计算 --> 制动扭矩匹配特性关系
-        JiHeHengNiuJuJiSuanMoXing,            // 数学模型 --> 恒扭矩计算 --> 恒扭矩计算模型
-        ShuXueHengNiuJuWangGeHuaFen,          // 数学模型 --> 恒扭矩计算 --> 网格划分
-        ShuXueHengNiuJuJiSuanCanShuShuRu,     // 数学模型 --> 恒扭矩计算 --> 计算参数输入
-        ShuXueHengNiuJuJiSuan,                // 数学模型 --> 恒扭矩计算 --> 计算
-        ShuXueHengNiuJuJiSuanJieGuoShuChu     // 数学模型 --> 恒扭矩计算 --> 计算参数输出
+        ShuXueZhiDongNiuJuPiPeiTeXingGuanXi,  // 数学模型 --> 制动特性计算模型 --> 制动扭矩匹配特性关系
+        JiHeHengNiuJuJiSuanMoXing,            // 数学模型 --> 制动特性计算模型 --> 制动特性计算模型模型
+        ShuXueHengNiuJuWangGeHuaFen,          // 数学模型 --> 制动特性计算模型 --> 网格划分
+        ShuXueHengNiuJuJiSuanCanShuShuRu,     // 数学模型 --> 制动特性计算模型 --> 计算参数输入
+        ShuXueHengNiuJuJiSuan,                // 数学模型 --> 制动特性计算模型 --> 计算
+        ShuXueHengNiuJuJiSuanJieGuoShuChu     // 数学模型 --> 制动特性计算模型 --> 计算参数输出
     }
 }
